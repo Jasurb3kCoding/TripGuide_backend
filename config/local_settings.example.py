251025d7cbf537ba93e9e816de8b56f28cc1794d
Muhammadjon <muhammadjon.200415@gmail.com>
2022-10-05 09:16:15 +0500

    fixed local setting database import error

--- config/local_settings.example.py
+++ config/local_settings.example.py
@@ -15,6 +15,6 @@ HOST = 'localhost:8000'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': 'db.sqlite3'
 #     }
 # }
