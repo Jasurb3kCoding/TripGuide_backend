6c246b3d60839cfd4ac6c9db339449e986feb8c3
Mirzohidpy <103629353+Mirzohidpy@users.noreply.github.com>
2022-10-05 13:59:44 +0500

    new commit

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Country, City, HotelFeature, HotelAmenity, HotelExtraFeature, Hotel
+from .models import Country, City, HotelFeature, HotelAmenity, HotelExtraFeature, Hotel, HotelImage, Review, \
+    RoomFeature, Room, RoomImage
 
 
 # Register your models here.
@@ -38,3 +39,42 @@ class HotelExtraFeatureAdmin(admin.ModelAdmin):
 class HotelAdmin(admin.ModelAdmin):
     list_display = ['title']
     fields = ['title', 'locatedCity', 'startCount', 'description', 'features', 'amenities', 'extra_features']
+
+
+@admin.register(HotelImage)
+class HotelImageAdmin(admin.ModelAdmin):
+    list_display = ['hotel']
+    fields = ['hotel', 'image']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['author']
+    fields = ['parent',
+              'author',
+              'hotel',
+              'message',
+              'startCount',
+              'created_date', ]
+
+
+admin.site.register(RoomFeature)
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    fields = ['hotel',
+              'title',
+              'price',
+              'features',
+              'is_discount',
+              'discount_rate', ]
+
+
+@admin.register(RoomImage)
+class RoomImageAdmin(admin.ModelAdmin):
+    list_display = ['room']
+    fields = ['image',
+              'room',
+              'order', ]
--- hotels/models.py
+++ hotels/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from account.models import User
 
 
 # Create your models here.
@@ -54,3 +55,51 @@ class Hotel(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class HotelImage(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.hotel
+
+
+class Review(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    message = models.TextField()
+    startCount = models.IntegerField()
+    created_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.author
+
+
+class RoomFeature(models.Model):
+    title = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.title
+
+
+class Room(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    price = models.CharField(max_length=80)
+    features = models.ForeignKey(RoomFeature, on_delete=models.CASCADE)
+    is_discount = models.BooleanField()
+    discount_rate = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+
+class RoomImage(models.Model):
+    image = models.ImageField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    order = models.IntegerField()
+
+    def __str__(self):
+        return self.room
